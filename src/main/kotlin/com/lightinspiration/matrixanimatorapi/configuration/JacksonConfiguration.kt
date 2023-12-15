bc74c88ab26a736a9824704539f55d585ecf5a42
Christopher Schmitz <cschmitz@labelinsight.com>
2023-12-15 11:47:25 -0600

    Cleans up test database configuration and tooling

--- src/main/kotlin/com/lightinspiration/matrixanimatorapi/configuration/JacksonConfiguration.kt
+++ src/main/kotlin/com/lightinspiration/matrixanimatorapi/configuration/JacksonConfiguration.kt
@@ -3,12 +3,10 @@ package com.lightinspiration.matrixanimatorapi.configuration
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
 
 @Configuration
 class JacksonConfiguration {
 
-    @Primary
     @Bean
     fun objectMapper(): ObjectMapper {
         return ObjectMapper()
--- src/test/kotlin/com/lightinspiration/matrixanimatorapi/configuration/TestDataSourceConfiguration.kt
+++ src/test/kotlin/com/lightinspiration/matrixanimatorapi/configuration/TestDataSourceConfiguration.kt
@@ -3,14 +3,14 @@ package com.lightinspiration.matrixanimatorapi.configuration
 import liquibase.Contexts
 import liquibase.LabelExpression
 import liquibase.Liquibase
+import liquibase.Scope
 import liquibase.database.DatabaseFactory
 import liquibase.database.jvm.JdbcConnection
-import liquibase.resource.FileSystemResourceAccessor
+import liquibase.resource.DirectoryResourceAccessor
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.jdbc.DataSourceBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
 import org.springframework.context.annotation.Profile
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.testcontainers.containers.PostgreSQLContainer
@@ -21,45 +21,85 @@ import javax.sql.DataSource
 @Profile("integration-test")
 class TestDataSourceConfiguration {
 
-    @Primary
+    // * Note that naming the bean and using its name as a qualifier in the namedParameterJdbcTemplate bean definition _isn't_ actually
+    // * necessary. Spring is smart enough to pull the correct DataSource bean. That said, I think it reads better to call it out.
     @Bean("test-postgres-datasource")
     fun testPostgresDataSource(): DataSource {
-        val postgresContainer =
-            PostgreSQLContainer("postgres:15-alpine")
-                .withUsername("postgres")
-                .withPassword("password")
-
-        postgresContainer.start()
-
-        val host = postgresContainer.host
-        val port = postgresContainer.firstMappedPort
-
-        val dataSource = DataSourceBuilder
-            .create()
-            .apply {
-                driverClassName("org.postgresql.Driver")
-                url("jdbc:postgresql://$host:$port/postgres")
-                username("postgres")
-                password("password")
-            }
-            .build()
-
-        val changeLog = File("src/test/resources/db/changelog/db.changelog-master.yaml")
-        // TODO: this works, but it's using deprecated tooling. figure out the current way of doing it
-        val liquibase = Liquibase(
-            changeLog.name,
-            FileSystemResourceAccessor(changeLog.parentFile),
-            DatabaseFactory.getInstance().findCorrectDatabaseImplementation(JdbcConnection(dataSource.connection))
-        )
-        liquibase.update(Contexts(), LabelExpression())
+        val containerConfig = startTestContainer(ContainerConfiguration("postgres", "password"))
+        val dataSource = configureDataSource(containerConfig)
+        buildSchema(dataSource)
 
         return dataSource
     }
 
     @Bean
-    @Primary
     fun namedParameterJdbcTemplate(@Qualifier("test-postgres-datasource") dataSource: DataSource): NamedParameterJdbcTemplate {
         return NamedParameterJdbcTemplate(dataSource)
     }
+
+    companion object {
+        fun startTestContainer(containerConfiguration: ContainerConfiguration): ContainerConfiguration {
+            val postgresContainer =
+                PostgreSQLContainer("postgres:15-alpine")
+                    .withUsername("postgres")
+                    .withPassword("password")
+
+            postgresContainer.start()
+
+            return containerConfiguration.copy(
+                host = postgresContainer.host,
+                port = postgresContainer.firstMappedPort
+            )
+        }
+
+        fun configureDataSource(containerConfiguration: ContainerConfiguration): DataSource {
+            return DataSourceBuilder
+                .create()
+                .apply {
+                    driverClassName("org.postgresql.Driver")
+                    url("jdbc:postgresql://${containerConfiguration.urlFormat}/postgres")
+                    username(containerConfiguration.username)
+                    password(containerConfiguration.password)
+                }
+                .build()
+        }
+
+        private fun buildSchema(dataSource: DataSource) {
+            Scope.child(
+                emptyMap()
+            ) { ->
+                // TODO: the path should prob be abstracted and passed in as a property
+                val changeLog = File("src/test/resources/db/changelog/db.changelog-master.yaml")
+                val liquibase = Liquibase(
+                    changeLog.name,
+                    DirectoryResourceAccessor(changeLog.parentFile),
+                    DatabaseFactory.getInstance()
+                        .findCorrectDatabaseImplementation(JdbcConnection(dataSource.connection))
+                )
+                liquibase.update(Contexts(), LabelExpression())
+                // * From what I'm reading this _should_ be the non-deprecated approach to starting and running liquibase,
+                // * but it doesn't work and I'm tired of fiddling with it :|
+                //val database =
+                //    DatabaseFactory.getInstance().findCorrectDatabaseImplementation(JdbcConnection(dataSource.connection))
+                //CommandScope(UpdateCommandStep.COMMAND_NAME.first())
+                //    .addArgumentValue(DbUrlConnectionCommandStep.DATABASE_ARG, database)
+                //    .addArgumentValue(
+                //        UpdateCommandStep.CHANGELOG_FILE_ARG,
+                //        "src/test/resources/db/changelog/db.changelog-master.yaml"
+                //    )
+                //    .execute()
+            }
+        }
+    }
 }
 
+
+data class ContainerConfiguration(
+    val username: String,
+    val password: String,
+    val host: String? = null,
+    val port: Int? = null
+) {
+    val urlFormat: String
+        get() = "$host:$port"
+}
